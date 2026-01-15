71edf35e4fe3b1f8ecc2323b0b98084a8718754e
Rachel.P <rachel.payette@edu.sait.ca>
2026-01-15 16:48:52 -0700

    Css and link amendments.
    adjusted global css variables (box shadow, deleted duplicates).
    
    adjusted links and css hover effects on projects page, fixed Linkedin links in footer and "about me" section.

---
+++ portfolioArray.js
@@ -0,0 +1,15 @@
+const Websites = [
+    {
+        projectName: 'Wild Rose Foundry - E-commerce',
+        mainImage: 'Assets/websites/WildRoseFoundry/wrf.Large.png',
+        mainAlt: 'A stylized rose made up of wood, stone, and metal. Wild rose symbol for Alberta, Canada.',
+        blurb: 'This site was created as a final project in one of my SAIT courses. The directive was to create an e-commerce website fron the ground up. <br><br> The site uses a database for recording products and vendors, storing user order data, and flagging items as `featured` or `default`.<br> <br> The time system `Temporal` was used in building the JS christmas countdown, but is currently only supported through Mozilla Firefox and not Google Chrome.',
+        SiteLink: 'https://dev.interactivedesign.ca/~rpayette/cprg310/final/',
+        extraPicA: 'Assets/websites/WildRoseFoundry/pottery.jpg',
+        extraPicB: 'Assets/websites/WildRoseFoundry/RoseIco-Dark.png',
+        extraPicC:'Assets/websites/WildRoseFoundry/RoseIco-Gold.png',
+        extraPicD:'Assets/websites/WildRoseFoundry/RoseIco-Med.png',
+        extraPicE:'Assets/websites/WildRoseFoundry/WRF.jpg'
+
+    }
+]
