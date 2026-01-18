c559845c08ef5788ce9bd751018faa48cc0c9dd1
Rachel.P <rachel.payette@edu.sait.ca>
2026-01-17 17:37:41 -0700

    Button ID, Back button, JS array
    Added artwork to assets/artwork folder. Adjusted buttons and added ID's where necessary for css targeting.
    Added back buttons to all main pages, other than index.
    begun working on the portfolio array .js, with a const array for each category. need to complete before making html replacing js script to run on details page.

--- portfolioArray.js
+++ portfolioArray.js
@@ -1,15 +1,149 @@
-const Websites = [
+// --------------------------Webites--------------------------
+const WebsiteArray = [
     {
         projectName: 'Wild Rose Foundry - E-commerce',
         mainImage: 'Assets/websites/WildRoseFoundry/wrf.Large.png',
         mainAlt: 'A stylized rose made up of wood, stone, and metal. Wild rose symbol for Alberta, Canada.',
         blurb: 'This site was created as a final project in one of my SAIT courses. The directive was to create an e-commerce website fron the ground up. <br><br> The site uses a database for recording products and vendors, storing user order data, and flagging items as `featured` or `default`.<br> <br> The time system `Temporal` was used in building the JS christmas countdown, but is currently only supported through Mozilla Firefox and not Google Chrome.',
         SiteLink: 'https://dev.interactivedesign.ca/~rpayette/cprg310/final/',
-        extraPicA: 'Assets/websites/WildRoseFoundry/pottery.jpg',
-        extraPicB: 'Assets/websites/WildRoseFoundry/RoseIco-Dark.png',
-        extraPicC:'Assets/websites/WildRoseFoundry/RoseIco-Gold.png',
-        extraPicD:'Assets/websites/WildRoseFoundry/RoseIco-Med.png',
-        extraPicE:'Assets/websites/WildRoseFoundry/WRF.jpg'
+        sitePicA: 'Assets/websites/WildRoseFoundry/pottery.jpg',
+        altA: 'a close-up photo of pottery',
+        sitePicB: 'Assets/websites/WildRoseFoundry/RoseIco-Dark.png',
+        altB: 'a simple black rose icon',
+        sitePicC:'Assets/websites/WildRoseFoundry/RoseIco-Gold.png',
+        altC: 'a simple gold rose icon',
+        sitePicD:'Assets/websites/WildRoseFoundry/RoseIco-Med.png',
+        altD: 'a simple copper rose icon',
+        sitePicE:'Assets/websites/WildRoseFoundry/WRF.jpg',
+        altE:'logo for Wild Rose Foundry. A stylized rose made out of wood, stone, and metal.'
+    },
+
+    {
+        projectName: 'Th3 Rice Lab - Nail Technician Site',
+        mainImage: 'Assets/websites/Th3RiceLab/Logo-V2.png',
+        mainAlt: 'A stylized Erlenmeyer flask with rainbow iridescent fluid and bubbles inside.',
+        blurb: 'This site was made for a local Calgarian nail technician. It has a full back-end and admin dashboard. The client needed a site to accompany her growth as a business and assist in filtering and scheduling clients and potential clients.',
+        SiteLink: 'https://th3ricelab.ca/',
+        sitePicA: 'Assets/websites/Th3RiceLab/TRL-more.jpg',
+        altA: 'A screen-shot of `Th3 Rice Lab` nail technician website, showing the main home page.',
+        sitePicB: 'Assets/websites/Th3RiceLab/TRL.jpg',
+        altB: 'A scren-shot of `Th3 Rice Lab` home page, showing several features available on the website.',
+        sitePicC:'Assets/websites/Th3RiceLab/v.3.png',
+        altC: 'An earlier version of `Th3 Rice Lab` logo with a more realistic/less stylized look.',
+        sitePicD:'Assets/websites/Th3RiceLab/Logo-V2.png',
+        altD: 'The logo for `Th3 Rice Lab`, an upright Erlenmeyer flask with rainbow liquid and bubbles inside.',
+        sitePicE:'Assets/websites/Th3RiceLab/S-S-chart.png',
+        altE:'A size and shape reference chart, in purple and black hues, made for `Th3 Rice Lab` website.'
+    }
+]
+// --------------------------Logos--------------------------
+const LogoArray = [
+    {
+        projectName: '',
+        mainImage: '',
+        mainAlt: '',
+        blurb: '',
+        logoA: '',
+        altA: '',
+        logoB: '',
+        altB: '',
+        logoC: '',
+        altC: '',
+        assetPic: '',
+        assetAlt: ''
+
+    },
+
+    {
+        projectName: '',
+        mainImage: '',
+        mainAlt: '',
+        blurb: '',
+        logoA: '',
+        altA: '',
+        logoB: '',
+        altB: '',
+        logoC: '',
+        altC: '',
+        assetPic: '',
+        assetAlt: ''
+
+    },
+
+     {
+        projectName: '',
+        mainImage: '',
+        mainAlt: '',
+        blurb: '',
+        logoA: '',
+        altA: '',
+        logoB: '',
+        altB: '',
+        logoC: '',
+        altC: '',
+        assetPic: '',
+        assetAlt: ''
+
+    },
+
+    {
+        projectName: '',
+        mainImage: '',
+        mainAlt: '',
+        blurb: '',
+        logoA: '',
+        altA: '',
+        logoB: '',
+        altB: '',
+        logoC: '',
+        altC: '',
+        assetPic: '',
+        assetAlt: ''
+
+    },
+
+    {
+        projectName: '',
+        mainImage: '',
+        mainAlt: '',
+        blurb: '',
+        logoA: '',
+        altA: '',
+        logoB: '',
+        altB: '',
+        logoC: '',
+        altC: '',
+        assetPic: '',
+        assetAlt: ''
+
+    },
+    {
+        projectName: '',
+        mainImage: '',
+        mainAlt: '',
+        blurb: '',
+        logoA: '',
+        altA: '',
+        logoB: '',
+        altB: '',
+        logoC: '',
+        altC: '',
+        assetPic: '',
+        assetAlt: ''
 
     }
+]
+// --------------------------Artwork--------------------------
+const ArtworkArray = [
+    {
+        projectName: '',
+        mainImage: '',
+        mainAlt: '',
+        blurb: '',
+        mediums: '',
+        picA: '',
+        altA: '',
+        picB: '',
+        altB: ''
+    },
 ]
