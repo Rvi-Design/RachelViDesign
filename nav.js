618bf89aee9d288e85e030b21992d4614317d4f1
Rachel.P <rachel.payette@edu.sait.ca>
2026-02-04 15:22:39 -0700

    index - partial mobile responsive + Skill section.
    Adjusted header, nav, footer, tagline, and About me section of the index page to look good on mobile.
    
    Downloaded more vectors, added section on skills.
    to redo project page

---
+++ nav.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('.siteNav');
+const menuToggle = document.getElementById('menuToggle');
+
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('is-open');
+});
