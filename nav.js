d987edc6b9a6b40f93fc02070463a6620e37a544
Rachel.P <rachel.payette@edu.sait.ca>
2026-02-05 08:47:25 -0700

    Includes and mobile front page
    Made the index page fully optimized for mobile. Added redundant code into includes.
    Will move onto changing to php next.
    uploaded all filed onto cpanel

--- nav.js
+++
@@ -1,6 +0,0 @@
-const nav = document.querySelector('.siteNav');
-const menuToggle = document.getElementById('menuToggle');
-
-menuToggle.addEventListener('click', () => {
-  nav.classList.toggle('is-open');
-});
